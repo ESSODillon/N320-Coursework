1bcf2a1e60e978f528cea169ac8be8204151bfd0
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2021-09-03 10:41:45 -0400

    Ground color change

--- labs/Ground_Rain/js/app.js
+++ labs/Ground_Rain/js/app.js
@@ -2,14 +2,87 @@ function setup() {
   createCanvas(600, 500);
 }
 
+let count = 0;
 let raindrops = [];
 let ground = [];
 let colors = ["#c0dbfc", "#408ec9", "#5c76b8", "#a1a6cf"];
-let count = 0;
-let ground_color = [138, 138, 138];
+let ground_color = {
+  r: 138,
+  g: 138,
+  b: 138,
+};
+
+class Raindrop {
+  constructor(x, y, radius, color) {
+    // Sets up the blueprints for making raindrops
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed = 1 + Math.random() * 3;
+    this.color = color;
+  }
+
+  preset() {
+    // Creates a circle
+    circle(this.x, this.y, this.radius);
+    // Colors the raindrop
+    fill(this.color);
+  }
+
+  fall() {
+    // Sets the speed for falling along the y axis
+    this.y = this.y + this.speed;
+
+    if (this.y >= 450) {
+      console.log(count);
+      count = count += 1;
+    }
+
+    if (count % 10 == 1) {
+      console.log("10 reached successfully");
+      ground_color.b += 7;
+      if (ground_color.b >= 200) {
+        ground_color.r += 10;
+        ground_color.g += 10;
+      }
+    }
+
+    // If this y position of the raindrop reaches 450 or greater, start back at the top of the screen.
+    if (this.y >= 450) {
+      this.y = 0;
+    }
+
+    // If speed is less than 15, accelerate. If it's greater than 15, slowdown.
+    if (this.speed <= 12) {
+      // Sets the acceleration of speed
+      this.speed = this.speed + 0.05;
+    } else {
+      this.speed == 12;
+    }
+  }
+}
+
+class Ground {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  create() {
+    fill(this.color);
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
 
 // Class instance for ground
-ground[0] = new Ground(0, 450, 600, 50, ground_color);
+ground[0] = new Ground(0, 450, 600, 50, [
+  ground_color.r,
+  ground_color.g,
+  ground_color.b,
+]);
 
 // Raindrop instances plugging into an array. If you want too add more raindrops, change the for loop.
 for (let i = 0; i <= 5; i++) {
