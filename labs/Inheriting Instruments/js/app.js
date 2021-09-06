3d81889cb843be029671d3127dea8674a30cf0ec
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2021-09-06 15:53:33 -0400

    Concrete Classes
    Created stringed instrument, woodwind and percussion

--- labs/Inheriting Instruments/js/app.js
+++ labs/Inheriting Instruments/js/app.js
@@ -1,14 +1,36 @@
 class Instrument {
-  constructor(family, play, loudness) {
+  constructor(family, verb, loudness) {
     this.family = family;
-    this.play = play;
+    this.verb = play;
     this.loudness = loudness;
   }
 
-  details() {
-    console.log(this.family, this.play, "at", this.loudness);
+  play() {
+    console.log(this.family, this.verb, this.loudness);
   }
 }
 
-let test = new Instrument("Guitar", "Strum", 200);
-test.details();
+class Woodwind extends Instrument {
+  constructor(family, verb, loudness) {
+    super(family, verb);
+    this.loudness = loudness;
+  }
+}
+
+class Percussion extends Instrument {
+  constructor(family, verb, loudness) {
+    super(family, verb);
+    this.loudness = loudness;
+  }
+}
+
+class Stringed extends Instrument {
+  constructor(family, verb, loudness) {
+    super(family, verb);
+    this.loudness = loudness;
+  }
+}
+
+let trumpet = new Woodwind("Trumpet", "toots", "ferociously");
+let bongos = new Percussion("Bongos", "slaps", "uproariously");
+let banjo = new Stringed("Banjo", "strums", "softly");
