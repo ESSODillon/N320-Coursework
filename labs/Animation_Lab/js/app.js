73e3fa2ac3f06a034ecb7c59b8b76de1ce8ad2b8
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2021-09-22 14:45:27 -0400

    Exit animation update

--- labs/Animation_Lab/js/app.js
+++ labs/Animation_Lab/js/app.js
@@ -1,13 +1,19 @@
+// Initialize all the HTML elements in my program
 let contentWrapper = document.getElementById("wrapper");
+let title = document.getElementById("title");
+let rectangle = document.getElementById("blue_rectangle");
+let alert = document.getElementById("alert");
+let alert_button = document.getElementById("alert_button");
 let row_squares = document.getElementsByClassName("row_square");
-let squares_array = ["one", "two", "three", "four", "five", "six"];
 
+// A build-in for all the elements (go from nothing on the screen, to everything visible)
 TweenMax.from(contentWrapper, {
   duration: 0.7,
   x: window.innerWidth,
   alpha: 0,
 });
 
+// A mouse over / highlight for each of the gallery elements below
 function highlight() {
   for (let i = 0; i < row_squares.length; i++) {
     TweenLite.from(row_squares[i], {
@@ -21,11 +27,45 @@ function highlight() {
   }
 }
 
+// A exit animation for when one gallery element is clicked
 function squareEscape() {
+  TweenLite.to(title, { duration: 1.5, x: window.innerWidth, display: "none" });
+
+  TweenLite.to(rectangle, {
+    duration: 1.5,
+    x: window.innerWidth,
+    display: "none",
+  });
+
+  TweenLite.from(alert, {
+    duration: 3,
+    x: window.innerWidth,
+    alpha: 0,
+  });
+
+  TweenLite.to(alert, {
+    display: "block",
+  });
+
+  TweenLite.from(alert_button, {
+    duration: 3,
+    x: window.innerWidth,
+    alpha: 0,
+  });
+
+  TweenLite.to(alert_button, {
+    display: "block",
+  });
+
   for (let i = 0; i < row_squares.length; i++) {
     TweenLite.to(row_squares[i], {
-      duration: 2.5,
-      backgroundColor: "purple",
+      duration: 1.5,
+      x: window.innerWidth,
+      display: "none",
     });
   }
+}
+
+function refreshPage() {
+  window.location.reload();
 }
